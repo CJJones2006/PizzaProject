7a388510ba0754d7d6332c4c5f8fd35304412089
celyn <celyn@CJ-PC>
2023-06-14 16:26:24 +0100

    a

--- PizzaProject.py
+++ PizzaProject.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from re import match
 import sqlite3
 
 #connection = sqlite3.connect("tutorial.db")
@@ -18,6 +19,9 @@ class Variety(Enum):
     Margherita = 0
     Pepperoni = 1
     Pineapple = 2
+    Meaty = 3
+    HamMushroom = 4
+    Garlic = 5
 
 class Pizza:
     def __init__(self, sizeInInches, variety):
@@ -25,8 +29,43 @@ class Pizza:
         self.variety = variety
         
         if self.variety == Variety.Margherita:
-            self.ingredients = ["Tomato Sauce", "Mozzerella", "Basil"]
-    
+            self.ingredients = ["Pizza dough", "Tomato Sauce", "Mozzerella", "Basil"]
+        elif self.variety == Variety.Pepperoni:
+            self.ingredients = ["Pizza dough", "Tomato Sauce", "Mozzerella", "Pepperoni"]
+        elif self.variety == Variety.Pineapple:
+            self.ingredients = ["Pizza dough", "Tomato Sauce", "Mozzerella", "Pineapple"]
+        elif self.variety == Variety.Meaty:
+            self.ingredients = ["Pizza dough", "Tomato Sauce", "Mozzerella", "Chicken", "Ham", "Beef"]
+        elif self.variety == Variety.HamMushroom:
+            self.ingredients = ["Pizza dough", "Tomato Sauce", "Mozzerella", "Ham", "Mushrooms"]
+        elif self.variety == Variety.Garlic:
+            self.ingredients = ["Pizza dough", "Mozzerella", "Garlic"]
+
+    def getCost(self):
+        if self.sizeInInches == 7:
+            cost = 5.00
+        elif self.sizeInInches == 10:
+            cost = 7.00
+        elif self.sizeInInches == 12:
+            cost = 8.00
+        elif self.sizeInInches == 14:
+            cost = 10.00
+        
+        if self.variety == Variety.Margherita:
+            cost += 0.00
+        elif self.variety == Variety.Pepperoni:
+            cost += 1.00
+        elif self.variety == Variety.Pineapple:
+            cost += 1.00
+        elif self.variety == Variety.Meaty:
+            cost += 2.00
+        elif self.variety == Variety.HamMushroom:
+            cost += 2.00
+        elif self.variety == Variety.Garlic:
+            cost += 0.50
+
+        return cost
+
 
 class Customer:
     def __init__(self, customerID, firstName, lastName, email, mobileNumber):
@@ -37,11 +76,10 @@ class Customer:
         self.mobileNumber = mobileNumber
 
 class CustomerOrders:
-    def __init__(self, orderNumber, cost, pizzas):
+    def __init__(self, customer, orderNumber, pizzas):
         self.orderNumber = orderNumber
-        self.cost = cost
         self.pizzas = pizzas
 
-margheritaPizza = Pizza(12, Variety.Margherita)
+pizza1 = Pizza(14, Variety.Garlic)
 
-print(margheritaPizza.ingredients)
+print(pizza1.getCost())
